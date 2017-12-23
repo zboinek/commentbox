0f4a7a3ea426640880ca0a8914a2adefe742d1d5
Kuba Z <zboinek2@gmail.com>
2017-12-23 17:42:16 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,7 @@
+//data.js
+const data = [
+    { id: 1, author: 'Bryan', text: 'Wow this is neat!'},
+    { id: 2, author: 'You', text: 'youre __right!__'},
+    { id:3, author: 'Kuba', text: 'Dziala dziala dziala'}
+   ]
+   module.exports = data;
